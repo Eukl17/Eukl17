9500ac77e06164f9a624114bdc0466736964c3fe
Pascal CHAUVIN <pascal.chauvin7@gmail.com>
2021-07-21 17:42:49 +0200

    Nettoyage et commentaire du code.

--- graphe-00/graphe.py
+++ graphe-00/graphe.py
@@ -9,15 +9,16 @@ import string
 
 
 def matrice_adj(n_sommets=8, n_aretes=6, alpha=True, oriente=False):
+	""" Construire aléatoirement un graphe par sa matrice d'adjacence. """
 	m = dict()
 
-	ok = (n_sommets in range(2, 26 + 1))
+	ok = (n_sommets in range(2, 26 + 1)) # au moins deux sommets
 
 	if ok:
 		ok = (n_aretes in range(n_sommets*(n_sommets - 1) // 2 + 1))
 
 	if not ok:
-		return m
+		return m # matrice rendue vide si erreur
 
 	if alpha:
 		noms = list(string.ascii_uppercase[0:n_sommets])
@@ -54,6 +55,8 @@ def matrice_adj(n_sommets=8, n_aretes=6, alpha=True, oriente=False):
 
 
 def liste_adj(m):
+	""" Construire la liste d'adjacence d'un graphe à partir de sa matrice 
+	    d'adjacence. """
 	l = dict()
 	for s in m:
 		l[s] = dict()
@@ -63,31 +66,29 @@ def liste_adj(m):
 	return l
 
 
-def afficher_graphe(g):
+def afficher_graphe(m):
+	""" Afficher un graphe donné par sa matrice d'adjacence. """
 	print("    ", end=" ")
-	for v in g:
+	for v in m:
 		print("{:>4}".format(v), end=" ")
 	print()
 
-	for v in g:
+	for v in m:
 		print("{:>4}".format(v), end=" ")
-		for w in g[v]:
-			print("{:>4}".format(g[v][w]), end=" ")
+		for w in m[v]:
+			print("{:>4}".format(m[v][w]), end=" ")
 		print()
 
 
 
 if __name__ == "__main__":
-	m = matrice_adj(26, 100) # premières lettres de l'alphabet, non orienté
+	m = matrice_adj(6, 8) # premières lettres de l'alphabet, non orienté
 
 	print("--- matrice d'adjacence ---")
-
 	afficher_graphe(m)
 
 	print("--- liste d'adjacence ---")
-
 	l = liste_adj(m)
 	for x in l:
 		print(x, l[x])
-
 
