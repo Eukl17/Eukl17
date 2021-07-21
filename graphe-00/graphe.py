f97a1a5fd9f8655108b4ab5dd0f79692b1a12b8e
Pascal CHAUVIN <pascal.chauvin7@gmail.com>
2021-07-21 15:30:34 +0200

    construire la liste d'adjacence (non terminé)

--- graphe-00/graphe.py
+++ graphe-00/graphe.py
@@ -8,7 +8,7 @@ import random
 import string
 
 
-def nouveau_graphe(nombre_sommets=8, oriente=False, alphabetique=False):
+def nouveau_graphe(nombre_sommets=8, nombre_aretes=6, oriente=False, alphabetique=False):
 	g = dict()
 
 	if not (nombre_sommets in range(2, 26)):
@@ -33,18 +33,25 @@ def nouveau_graphe(nombre_sommets=8, oriente=False, alphabetique=False):
 
 	sommets = sorted(sommets)
 
+	k = 0
 	for s in sommets:
 		g[s] = dict()
 		for t in sommets:
-			if s is t:
-				g[s][t] = 0 # "-"
-			else:
-				if oriente:
-					g[s][t] = random.choice(range(0, 10))
+			if k < nombre_aretes:
+				if s is t:
+					g[s][t] = 0 # "-"
 				else:
-					if s > t:
-						g[s][t] = random.choice(range(0, 10))
-						g[t][s] = g[s][t]
+					if oriente:
+						g[s][t] = random.choice(range(1, 10))
+						k += 1
+					else:
+						if s > t:
+							g[s][t] = random.choice(range(1, 10))
+							g[t][s] = g[s][t]
+							k += 1
+			else:
+				g[s][t] = 0
+
 	return g
 
 
@@ -61,9 +68,24 @@ def afficher_graphe(g):
 		print()
 
 
+def liste_adjacence(m):
+	l = dict()
+	for s in m:
+		l[s] = dict()
+		for t in m[s]:
+			if m[s][t] != 0:
+				l[s][t] = m[s][t]
+	return l
+
+
+
 if __name__ == "__main__":
-	g = nouveau_graphe(6, False, True)
+	g = nouveau_graphe(6, 4, False, True)
 	afficher_graphe(g)
+
+	l = liste_adjacence(g)
+	for x in l:
+		print(x, l[x])
 
 
 #        B    C    E    F    H    K    O    S 
@@ -85,4 +107,18 @@ if __name__ == "__main__":
 #   T    9    5    3    4    0    2 
 #   X    5    2    2    6    1    0 
 
+
+#        A    B    C    D    E    F 
+#   A    0    3    4    0    9    5 
+#   B    3    0    3    4    0    2 
+#   C    4    3    0    6    9    7 
+#   D    0    4    6    0    5    4 
+#   E    9    0    9    5    0    5 
+#   F    5    2    7    4    5    0 
+#A {'B': 3, 'C': 4, 'E': 9, 'F': 5}
+#B {'A': 3, 'C': 3, 'D': 4, 'F': 2}
+#C {'A': 4, 'B': 3, 'D': 6, 'E': 9, 'F': 7}
+#D {'B': 4, 'C': 6, 'E': 5, 'F': 4}
+#E {'A': 9, 'C': 9, 'D': 5, 'F': 5}
+#F {'A': 5, 'B': 2, 'C': 7, 'D': 4, 'E': 5}
 
